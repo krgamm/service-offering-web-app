7b283cf8047e55787ddba64633d38bd6bae87398
Kevin Gammenthaler <kevin.gammenthaler@gmail.com>
2021-11-22 12:08:00 -0600

    initial commit

---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '73717834-c1d8-4171-8967-9e6d8cfc7d0f'
+};
---
+++ frontend/src/_data/services.js
@@ -0,0 +1,24 @@
+const Cache = require("@11ty/eleventy-cache-assets");
+require("dotenv").config();
+
+async function fetchData(apiPath) {
+  const url = `http://localhost:1337${apiPath}`;
+
+  try {
+    let json = await Cache(url, {
+      duration: "5s",
+      type: "json",
+    });
+    return json;
+  } catch (e) {
+    console.error(`[API] error fetching ${url}`);
+    return null;
+  }
+}
+
+module.exports = async function () {
+  // TODO: make a forEach apiPaths
+  const serviceData = await fetchData("/services");
+  console.log(serviceData);
+  return serviceData;
+};
