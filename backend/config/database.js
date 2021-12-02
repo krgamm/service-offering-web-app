3460b888b4978825325a3abe9d89e8bfec38312c
Kevin Gammenthaler <kevin.gammenthaler@gmail.com>
2021-12-02 13:29:39 -0600

    new commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,19 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+        client: "postgres",
+        host: env("DATABASE_HOST"),
+        port: env.int("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
+        ssl: env.bool("DATABASE_SSL"),
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
 });
