03eec9dc5efa9f0aeb98c778971b9f629870af53
Kevin Gammenthaler <kevin.gammenthaler@gmail.com>
2021-12-02 16:52:33 -0600

    modal updates

--- frontend/src/_data/services.js
+++ frontend/src/_data/services.js
@@ -1,23 +1,23 @@
-const Cache = require("@11ty/eleventy-cache-assets");
-require("dotenv").config();
+// const Cache = require("@11ty/eleventy-cache-assets");
+// require("dotenv").config();
 
-async function fetchData(apiPath) {
-  const url = `http://localhost:1337${apiPath}`;
+// async function fetchData(apiPath) {
+//   const url = `http://localhost:1337${apiPath}`;
 
-  try {
-    let json = await Cache(url, {
-      duration: "5s",
-      type: "json",
-    });
-    return json;
-  } catch (e) {
-    console.error(`[API] error fetching ${url}`);
-    return null;
-  }
-}
+//   try {
+//     let json = await Cache(url, {
+//       duration: "5s",
+//       type: "json",
+//     });
+//     return json;
+//   } catch (e) {
+//     console.error(`[API] error fetching ${url}`);
+//     return null;
+//   }
+// }
 
-module.exports = async function () {
-  // TODO: make a forEach apiPaths
-  const services = await fetchData("/services");
-  return services;
-};
+// module.exports = async function () {
+//   // TODO: make a forEach apiPaths
+//   const services = await fetchData("/services");
+//   return services;
+// };
